704ce889a9451dd291e60e5eff6b2d1d65ddae96
wsantostorres <santos.torres@escolar.ifrn.edu.br>
2022-12-02 14:45:17 -0300

    mudança no layout do sistema

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,23 +1,38 @@
+window.addEventListener('DOMContentLoaded', event => {
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+
 function showActivePage(){
     let url_atual = window.location.href;
-    let itemTopics = document.querySelectorAll('#topics li');
-    let linkTopics = document.querySelectorAll('#topics li a');
+    let linkTopics = document.querySelectorAll('#topics a');
     
-    for(i = 0; i < itemTopics.length; i++){
+    for(i = 0; i < linkTopics.length; i++){
         let link = linkTopics[i].href;
-        if(url_atual.indexOf(link) != -1){
-            itemTopics[i].classList.toggle('active')
+        if(url_atual.indexOf(link) != -1 && url_atual == link){
+            linkTopics[i].classList.toggle('active')
         }
     }
 }
 
+var form = document.getElementById('form-search');
+var search = document.getElementById('input-search');
+
+function borderColor(){
+    form.classList.add('focus');
+}
+
+function removeborderColor(){
+    form.classList.remove('focus');
+}
+
 addEventListener('load', showActivePage);
-
-
-
-
-
-
-
-
-
+search.addEventListener('focus', borderColor);
+search.addEventListener('blur', removeborderColor);
